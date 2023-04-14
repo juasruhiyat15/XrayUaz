2ac81f996c41c15a2d947bf462aa5b084ea6ecb9
Juas Ruhiyat Akbar <72286824+juasruhiyat15@users.noreply.github.com>
2023-04-14 08:42:42 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ Version=$(curl -sS https://raw.githubusercontent.com/juasruhiyat15/XrayUaz/main_
 if [ $LocalVersion = $Version ]; then
 echo -e "\e[32mScript Sudah Versi Terbaru...\e[0m"
 else
-echo -e "\e[32mAda Update Script!!\e[0m";
+# echo -e "\e[32mAda Update Script!!\e[0m";
 # wget -q -O updateyes.sh https://raw.githubusercontent.com/juasruhiyat15/XrayUaz/main_juasrhiyat15/update/updateyes.sh && chmod +x updateyes.sh && ./updateyes.sh
 rm -f updateyes.sh
 fi
